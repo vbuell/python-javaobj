e4562ada0aefa9357488e9c6871b80a474230f73
Volodymyr Buell <vbuell@gmail.com>
2013-09-15 17:52:25 +0300

    LICENSE added

--- javaobj.py
+++ javaobj.py
@@ -1,8 +1,24 @@
-"""Provides functions for reading and writing (writing is WIP currently) Java objects
-serialized or will be deserialized by ObjectOutputStream. This form of object
+#   Copyright 2010-2013 Volodymyr Buell
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+   
+"""
+Provides functions for reading (writing is WIP currently) of Java 
+objects serialized by ObjectOutputStream. This form of object
 representation is a standard data interchange format in Java world.
 
-javaobj module exposes an API familiar to users of the standard library marshal, pickle and json modules.
+javaobj module exposes an API familiar to users of the standard modules 
+such as marshal, pickle and json.
 
 See: http://download.oracle.com/javase/6/docs/platform/serialization/spec/protocol.html
 """
